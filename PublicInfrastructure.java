9e0caf8998f644d76f7cc02174b12e8c98bb3e5c
Roger Cheng <rc729@cornell.edu>
2017-08-05 18:38:22 -0400

    Update PublicInfrastructure.java

--- PublicInfrastructure.java
+++ PublicInfrastructure.java
@@ -2,6 +2,13 @@ package finalProject;
 
 import java.util.LinkedList;
 
+/**
+ * Public infrastructure is a base class for Road and Village. It has VIP and capacity.
+ * VIP is the required status of gnomes to utilize this infrastructure. Capacity is the 
+ * maximum number of gnomes that can utilize the infrastructure at any time.
+ * 
+ */
+
 public class PublicInfrastructure {
 	protected int capacity, VIP;
 	protected LinkedList<Gnome> gnomes;
@@ -28,6 +35,12 @@ public class PublicInfrastructure {
 		return gnomes.size();
 	}
 	
+	/**
+	 * Add gnomes to the infrastructure. If the gnome vip is not high enough
+	 * or the capacity of the infrastructure has been reached, throw exception.
+	 * @param gnome
+	 * @throws Exception
+	 */
 	public void addGnome(Gnome gnome) throws Exception {
 		if (gnomes.size() == capacity)
 			throw new Exception("Infrastructure is full!");
